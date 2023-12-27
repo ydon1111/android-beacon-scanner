21292571a1695f395b51f78ffdb52d4cf8054a93
yeongdon <kyd5022@naver.com>
2023-12-27 18:19:51 +0900

    fix: RoomDatabase 수정중
    Roomdatabë 확인중

--- app/src/main/java/com/example/android_beacon_scanner/MyApplication.kt
+++ app/src/main/java/com/example/android_beacon_scanner/MyApplication.kt
@@ -5,20 +5,20 @@ import android.content.Context
 import android.util.Log
 import androidx.room.Room
 import com.example.android_beacon_scanner.room.AppDatabase
+import com.example.android_beacon_scanner.room.DeviceDataDao
 import dagger.hilt.android.HiltAndroidApp
+
 
 
 @HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        val appDatabase = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "device_data"
-        ).build()
 
-        if (appDatabase.isOpen) {
+        // Initialize the Room database instance
+        val appDatabase = AppDatabase.getInstance(this)
+
+        if (appDatabase != null) {
             Log.d("MyApplication", "Room database created successfully")
         } else {
             Log.e("MyApplication", "Failed to create Room database")
--- app/src/main/java/com/example/android_beacon_scanner/room/AppDatabase.kt
+++ app/src/main/java/com/example/android_beacon_scanner/room/AppDatabase.kt
@@ -1,11 +1,40 @@
 package com.example.android_beacon_scanner.room
 
+import android.content.Context
+import android.util.Log
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 
 
 @Database(entities = [DeviceRoomData::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun deviceDataDao(): DeviceDataDao
+    // 데이터 베이스 객체를 싱글톤으로 인스턴스.
+    companion object {
+        private var instance: AppDatabase? = null
 
+        @Synchronized
+        fun getInstance(context: Context): AppDatabase? {
+            if (instance == null)
+                synchronized(AppDatabase::class) {
+                    instance = databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "device_data"
+                    )
+                        .build()
+                }
+            return instance
+        }
+
+        fun destroyInstance() {
+            instance = null
+        }
+    }
 }
+
+
+
+
